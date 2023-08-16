f7ed9b653334ab1369579f73be82aa5e064dfa60
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-08-15 19:21:48 -0700

    add sbatch

---
+++ sbatch_alignment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -D /home/ajmuhich/alignment
+#SBATCH -o /home/ajmuhich/slurm-log/alignment_stdout-%j.txt
+#SBATCH -e /home/ajmuhich/slurm-log/alignment_stderr-%j.txt
+#SBATCH -J alignment
+#SBATCH -t 90:00:00
+#SBATCH --mem 50G
+#SBATCH -n 8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ajmuhich@ucdavis.edu
+bash /home/ajmuhich/scripts/alignment
