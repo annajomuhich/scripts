a064fa06174d6133d4ce59ec934afa3a0e5febbf
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-08-21 07:43:24 -0700

    --no-save to R script commands

--- sbatch_alignment.sh
+++ sbatch_alignment.sh
@@ -15,7 +15,7 @@ bash scripts/2_alignment.sh
 #generate alignment_summary.csv
 bash scripts/2a_alignment_summary1.sh
 module load R
-R scripts/2b_alignment_summary2.R
+R scripts/2b_alignment_summary2.R --no-save
 
 #generate readcounts from .bams
-R scripts/3_readcounts.R
+R scripts/3_readcounts.R --no-save
