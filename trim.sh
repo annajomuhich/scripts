4886b22da66a4426320b48e143a7f8e54bae0126
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-09-09 18:17:53 -0700

    removing paths from trimmomatic command

--- trim.sh
+++ trim.sh
@@ -8,15 +8,15 @@ conda activate trimmomatic
 #conda install -c bioconda trimmomatic
 
 ### Generate a list of the samples you want to run and save as file_list.txt
-cd fastq2readcounts
+cd ~/fastq2readcounts
 #assign your file_list.txt to files variable
 readarray -t files < file_list.txt
 
-### Get fastq files
+### Get raw fastq files
 # set up symlink to your fastq files that is hosted in shared lab directory
 #mkdir /group/kliebengrp/ajmuhich/raw_fastq
 #ln -s /group/kliebengrp/ajmuhich/raw_fastq raw_fastq
-#cd fastq
+#cd raw_fastq
 # loop to download fastq files using your file list
 #for file in "${files[@]}"
 #do
@@ -27,23 +27,28 @@ readarray -t files < file_list.txt
 # unzip the files
 #gunzip *.fastq.gz
 
+### Make directories for processed fastqs
+cd ~/fastq2readcounts
 mkdir /group/kliebengrp/ajmuhich/fastq
 ln -s /group/kliebengrp/ajmuhich/fastq fastq
-
 mkdir /group/kliebengrp/ajmuhich/unpaired_fastq
 ln -s /group/kliebengrp/ajmuhich/unpaired_fastq unpaired_fastq
 
 ### run trimmomatic
+cd raw_fastq
 for file in "${files[@]}"
 do
   echo ' '
   echo 'Trimming' $file '...'
   echo ' '
-  trimmomatic PE -threads 8 /raw_fastq/${file}_R1.fastq /raw_fastq/${file}_R2.fastq \
-  /fastq/${file}_R1_trimmed_paired.fastq /unpaired_fastq/${file}_R1_trimmed_unpaired.fastq \
-  /fastq/${file}_R2_trimmed_paired.fastq /unpaired_fastq${file}_R2_trimmed_unpaired.fastq \
+  trimmomatic PE -threads 8 ${file}_R1.fastq ${file}_R2.fastq \
+  ${file}_R1_trimmed_paired.fastq ${file}_R1_trimmed_unpaired.fastq \
+  ${file}_R2_trimmed_paired.fastq ${file}_R2_trimmed_unpaired.fastq \
   ILLUMINACLIP:reference/adapters.fa:2:30:10 \
   LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
+  #relocate output fastqs
+  mv *paired.fastq ~/fastq2readcounts/fastq
+  mv *unpaired.fastq ~/fastq2readcounts/unpaired_fastq
 done
 
 ### compress unused fastqs to conserve space
