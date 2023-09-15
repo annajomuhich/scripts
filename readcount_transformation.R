7b98e6f5fda086bd26cade1b6d97f9e6af34d75c
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-09-15 11:26:06 -0700

    initial commit

---
+++ readcount_transformation.R
@@ -0,0 +1,76 @@
+########### Readcount transformation and TMM normalization
+########### September 2023
+########### Anna Jo Muhich
+
+library(tidyverse)
+library(ggplot2)
+library(edgeR)
+
+#read in data
+host <- read.csv("Bos_1/readcounts/Host_readcounts.csv")
+bcin <- read.csv("Bos_1/readcounts/Bcin_readcounts.csv")
+
+#clean up transcript names
+bcin$transcript <- sub("^transcript:", "", bcin$transcript)
+
+#remove noncoding RNA for botrytis
+bcin <- bcin %>% filter(!grepl("^E", transcript))
+
+##### Collapse exon reads into total reads for each gene
+#make gene column
+host <- host %>%
+	mutate(gene = sub("\\.[0-9].exon[0-9]+$", "", transcript)) %>%
+	select(transcript, gene, everything())
+bcin <- bcin %>%
+	mutate(gene = sub("\\.[0-9]+$", "", transcript)) %>%
+	select(transcript, gene, everything())
+#add together counts within each gene for each sample
+host <- host %>%
+	group_by(gene) %>%
+	summarise(across(starts_with("B"), sum)) #starts_with("B") because my sample IDs are e.g. "B27_1"
+bcin <- bcin %>%
+	group_by(gene) %>%
+	summarise(across(starts_with("B"), sum))
+
+#check colnames match
+colnames(host) == colnames(bcin)
+
+#bind readcounts together
+df <- rbind(host, bcin)
+
+#remove genes with no counts across any sample.
+df <- df[rowSums(df[, -1] ==0) != ncol(df) - 1, ]
+
+##### reformat as matrix where colnames are samples, rownames are genes, and cells contain numeric read counts
+count_data <- df
+#remove spaces from the readcounts
+count_data[, -1] <- apply(count_data[, -1], 2, function(x) trimws(x))
+#remove gene column
+count_data <- count_data[,-1]
+#convert readcounts to numeric values
+count_data[!is.numeric(count_data)] <- lapply(count_data[!is.numeric(count_data)], as.numeric)
+#convert to matrix
+count_data <- as.matrix(count_data)
+#put in gene rownames
+row.names(count_data) <- df$gene
+
+# Create a DGEList object
+dge <- DGEList(counts = count_data)
+
+# #Filter Low-expressed genes (not sure if I want to do this yet)
+# keep <- rowSums(cpm(dge) >= 1) >= 2
+# dge <- dge[keep, ]
+
+#perform normalization using TMM
+dge <- calcNormFactors(dge)
+
+#access normalization factors
+normalization_factors <- dge$samples$norm.factors
+
+#extract normalized counts
+normalized_counts <- cpm(dge, normalized.lib.sizes = T)
+normalized_counts <- as.data.frame(normalized_counts)
+normalized_counts <- rownames_to_column(normalized_counts,var = "gene")
+
+#write csv
+write.csv(normalized_counts, "Bos_1/readcounts/norm_counts_all.csv", row.names = F, col.names = T)
