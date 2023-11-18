3859a4fb8d63f4f5dc1f634611ee18197a660272
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-11-18 15:30:06 -0800

    fix adapter path

--- trim.sh
+++ trim.sh
@@ -18,17 +18,16 @@ readarray -t files < file_list.txt
 mkdir /group/kliebengrp/ajmuhich/raw_fastq
 ln -s /group/kliebengrp/ajmuhich/raw_fastq raw_fastq
 #put adapters.fa into raw_fastq
-cp /reference/adapters.fa raw_fastq
+cp reference/adapters.fa raw_fastq
 cd raw_fastq
 # loop to download fastq files using your file list
-for file in "${files[@]}"
-do
-  # Download R1 and R2 for the sample. Change path as needed
-  wget -nv "http://slimsdata.genomecenter.ucdavis.edu/Data/32pbd2fb7i/Unaligned/Project_DKAM_BOS_2/${file}_R1.fastq.gz"
-  wget -nv "http://slimsdata.genomecenter.ucdavis.edu/Data/32pbd2fb7i/Unaligned/Project_DKAM_BOS_2/${file}_R2.fastq.gz"
-done
+#do
+#  # Download R1 and R2 for the sample. Change path as needed
+#  wget -nv "http://slimsdata.genomecenter.ucdavis.edu/Data/32pbd2fb7i/Unaligned/Project_DKAM_BOS_2/${file}_R1.fastq.gz"
+#  wget -nv "http://slimsdata.genomecenter.ucdavis.edu/Data/32pbd2fb7i/Unaligned/Project_DKAM_BOS_2/${file}_R2.fastq.gz"
+#done
 # unzip the files
-gunzip *.fastq.gz
+#gunzip *.fastq.gz
 
 ### Make directories for processed fastqs
 cd ~/fastq2readcounts
