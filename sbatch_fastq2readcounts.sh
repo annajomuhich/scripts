d0d2f7d50b2c67b50a41d7aac80c67646e504123
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2023-12-19 09:52:25 -0800

    just readcounts

--- sbatch_fastq2readcounts.sh
+++ sbatch_fastq2readcounts.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ajmuhich@ucdavis.edu
 
-module load conda
+#module load conda
 
 #run trimmomatic
 #bash scripts/trim.sh
@@ -18,12 +18,12 @@ module load conda
 #bash scripts/1_QC.sh
 
 #run alignments
-bash scripts/2_alignment.sh
+#bash scripts/2_alignment.sh
 
 #generate alignment_summary.csv
-bash scripts/2a_alignment_summary1.sh
+#bash scripts/2a_alignment_summary1.sh
 module load R
-R CMD BATCH scripts/2b_alignment_summary2.R
+#R CMD BATCH scripts/2b_alignment_summary2.R
 
 #generate readcounts from .bams
 R CMD BATCH scripts/3_readcounts.R
