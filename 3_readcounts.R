c001ec5cb9492ba1c83216dbbbbb5214b57437f6
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2024-02-26 15:15:08 -0800

    araport11 gtf reference

--- 3_readcounts.R
+++ 3_readcounts.R
@@ -74,7 +74,7 @@ for (subdir in subdirs) {
       
       # Run featureCounts on the file with Host genes
       count_matrix <- featureCounts(file = file_name,
-                                    annot.ext = "~/fastq2readcounts/reference/Monoe_Viroflay_gene.gtf",
+                                    annot.ext = "~/fastq2readcounts/reference/Araport11_GTF_genes_transposons.current.gtf",
                                     isGTFAnnotationFile = TRUE,
                                     isPairedEnd = TRUE)
       # Assign the count matrix to an object with a name based on the file name
