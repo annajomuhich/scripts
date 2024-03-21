c1880e80312f6a9b4a82fcc7ed52ba3c78e6a54e
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2024-03-21 14:06:06 -0700

    set up for caroline

--- tar.sh
+++ tar.sh
@@ -1,12 +1,12 @@
-cd /group/kliebengrp/ajmuhich/EUTC_spinach
+cd /group/kliebengrp/ajmuhich/Caroline_RNAseq
 echo "compressing fastq 1/3"
 tar -czvf fastq.tar.gz fastq
 rm -r fastq
 
-#echo "compressing unpaired_fastq"
-#tar -czvf unpaired_fastq.tar.gz unpaired_fastq
-#rm -r unpaired_fastq
+echo "compressing unpaired_fastq"
+tar -czvf unpaired_fastq.tar.gz unpaired_fastq
+rm -r unpaired_fastq
 
-#echo "compressing raw_fastq"
-#tar -czvf raw_fastq.tar.gz raw_fastq
-#rm -r raw_fastq
+echo "compressing raw_fastq"
+tar -czvf raw_fastq.tar.gz raw_fastq
+rm -r raw_fastq
